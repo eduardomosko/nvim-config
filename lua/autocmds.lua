ac8a97654ca5a3d34c79167cfc2dc2c52ec0a855
Eduardo Mosko <eduardommosko@gmail.com>
2022-02-05 11:43:04 -0300

    Removed improvised gofmt

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -8,7 +8,3 @@ autocmd "BufWritePost plugins.lua PackerCompile"
 -- AutoReload config on edit
 autocmd 'BufWritePost ~/.config/nvim/**/*.lua luafile %'
 
--- GoFmt
-autocmd "BufWritePost *.go silent !go run golang.org/x/tools/cmd/goimports -w %"
-autocmd "BufWritePost *.go e"
-
