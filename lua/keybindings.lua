81f599789acf7f618159a6984b5350ab259bd62c
Eduardo Mosko <eduardommosko@gmail.com>
2022-02-05 14:10:46 -0300

    Reorganized keybingins

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,66 +1,58 @@
 local map = vim.api.nvim_set_keymap
 
-local function nmap(mapping)
-	local pos = mapping:find(' ')
-	local kb = mapping:sub(0, pos - 1)
-	local cmd = mapping:sub(pos + 1)
-	map('n', kb, cmd, {silent = true, noremap = true})
-end
+local nmap = function(kb, cmd) map('n', kb, cmd, {silent = true, noremap = true}) end
+local tmap = function(kb, cmd) map('t', kb, cmd, {silent = true, noremap = true}) end
 
 vim.g.mapleader = ' '
 
-local nrm = { silent = true, noremap = true }
-
-nmap '<leader>t <cmd>NERDTreeToggle<cr>'
+nmap('<leader>t', '<cmd>NERDTreeToggle<cr>')
 
 -- Navigation
-map('n', '<leader>h', '<C-w>h', nrm)
-map('n', '<leader>j', '<C-w>j', nrm)
-map('n', '<leader>k', '<C-w>k', nrm)
-map('n', '<leader>l', '<C-w>l', nrm)
+nmap('<leader>h', '<C-w>h')
+nmap('<leader>j', '<C-w>j')
+nmap('<leader>k', '<C-w>k')
+nmap('<leader>l', '<C-w>l')
 
-map('n', 'çh', ':bp<cr>', nrm)
-map('n', 'çl', ':bn<cr>', nrm)
-map('n', 'çk', ':tabn<cr>', nrm)
-map('n', 'çj', ':tabp<cr>', nrm)
+nmap('çh', ':bp<cr>')
+nmap('çl', ':bn<cr>')
+nmap('çk', ':tabn<cr>')
+nmap('çj', ':tabp<cr>')
 
 -- Terminal life
-nmap '<leader>ot :bot 15sp | term<enter>A'
-map('t', '<esc>', '<C-\\><C-n>', nrm)
+nmap('<leader>ot', ':bot 15sp | term<enter>A')
+tmap('<esc>', '<C-\\><C-n>')
 
 -- GitGutter
 --  Disable old
-nmap '<leader>_a <Plug>(GitGutterStageHunk)'
-nmap '<leader>_b <Plug>(GitGutterUndoHunk)'
-nmap '<leader>_c <Plug>(GitGutterPreviewHunk)'
+nmap('<leader>_a', '<Plug>(GitGutterStageHunk)')
+nmap('<leader>_b', '<Plug>(GitGutterUndoHunk)')
+nmap('<leader>_c', '<Plug>(GitGutterPreviewHunk)')
 
 --  Enable new
-nmap '<leader>gs <cmd>GitGutterStageHunk<cr>'
-nmap '<leader>gu <cmd>GitGutterUndoHunk<cr>'
-nmap '<leader>gp <cmd>GitGutterPreviewHunk<cr>'
-nmap '<leader>gn <cmd>GitGutterNextHunk<cr>'
-nmap '<leader>gN <cmd>GitGutterPrevHunk<cr>'
+nmap('<leader>gs', '<cmd>GitGutterStageHunk<cr>')
+nmap('<leader>gu', '<cmd>GitGutterUndoHunk<cr>')
+nmap('<leader>gp', '<cmd>GitGutterPreviewHunk<cr>')
+nmap('<leader>gn', '<cmd>GitGutterNextHunk<cr>')
+nmap('<leader>gN', '<cmd>GitGutterPrevHunk<cr>')
 
 -- Telescope
-nmap "<leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>"
-nmap "<leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>"
+nmap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
+nmap("<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
 
 -- Closing files
-nmap 'ZA <cmd>qa<cr>'
+nmap('ZA', '<cmd>qa<cr>')
 
 -- Lsp
-function lsp(client, bufnr)
-	local bmap = function(kb, cmd)
-		vim.api.nvim_buf_set_keymap(bufnr, 'n', kb, cmd, nrm)
-	end
-
-	bmap('<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>')
-	bmap('<leader>gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-	bmap(']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>')
-	bmap('[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>')
-	bmap('K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-end
-
 return {
-	lsp = lsp,
+	lsp = function(client, bufnr)
+		local bmap = function(kb, cmd)
+			vim.api.nvim_buf_set_keymap(bufnr, 'n', kb, cmd, nrm)
+		end
+
+		bmap('<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>')
+		bmap('<leader>gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+		bmap(']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<cr>')
+		bmap('[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>')
+		bmap('K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+	end,
 }
--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,4 +1,15 @@
 require('nvim-treesitter.configs').setup {
-	ensure_installed = {'c', 'lua', 'javascript', 'typescript', 'bash', 'python', 'yaml', 'json', 'go', 'cpp'},
+	ensure_installed = {
+		'c',
+		'lua',
+		'javascript',
+		'typescript',
+		'bash',
+		'python',
+		'yaml',
+		'json',
+		'go',
+		'cpp'
+	},
 	highlight = { enable = true }
 }
