1852a64260907a7764d63f8cea5d2c45751b829a
Eduardo Mosko <eduardommosko@gmail.com>
2022-02-18 13:23:55 -0300

    Uses formatters to format current buffer

---
+++ lua/formatter.lua
@@ -0,0 +1,47 @@
+local CURRENT_BUF = 0
+
+local formatters = {
+	rust = 'rustfmt --edition "2021"',
+	go = 'go run golang.org/x/tools/cmd/goimports@latest',
+	svelte = 'prettier --parser svelte',
+	javascript = 'prettier --parser flow',
+	typescript = 'prettier --parser typescript',
+	json = 'prettier --parser json',
+	css = 'prettier --parser css',
+}
+
+-- Compares every item of a table
+local function equals(l1, l2) 
+	if #l1 ~= #l2 then
+		return false
+	end
+
+	for i = 0, #l1 do
+		if l1[i] ~= l2[i] then
+			return false
+		end
+	end
+
+	return true
+end
+
+return {
+	format = function()
+		local ft = vim.bo.filetype
+		local format = formatters[ft]
+	
+		if format ~= nil then
+			local oldlines = vim.api.nvim_buf_get_lines(CURRENT_BUF, 0, -1, true)
+	
+			local newlines = vim.fn.systemlist(format, oldlines)
+			if vim.v.shell_error ~= 0 then
+				print(table.concat(newlines, '\n'))
+				return
+			end
+	
+			if not equals(newlines, oldlines) then
+				vim.api.nvim_buf_set_lines(CURRENT_BUF, 0, -1, true, newlines)
+			end
+		end
+	end,
+}
