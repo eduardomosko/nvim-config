1a160e3abce771d76fb564443b655aa00f934520
Eduardo Mosko <eduardommosko@gmail.com>
2022-12-26 13:12:58 -0300

    using my fork of tree-sitter-svelte

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,3 +1,14 @@
+-- Uses patched version of parser
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+parser_config.svelte = {
+	install_info = {
+		url = "https://github.com/eduardomosko/tree-sitter-svelte",
+		branch = "fix-empty-await-then",
+		files = { "src/parser.c", "src/scanner.c" },
+		requires_generate_from_grammar = false,
+	}
+}
+
 require('nvim-treesitter.configs').setup {
 	ensure_installed = {
 		'c',
@@ -11,6 +22,7 @@ require('nvim-treesitter.configs').setup {
 		'go',
 		'cpp',
 		'css',
+		'html',
 	},
 	highlight = { enable = true }
 }
