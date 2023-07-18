b3912d8f8bf809e6f95aa28168c024a13b6d351b
Eduardo Mosko <eduardo.mosko@zup.com.br>
2023-07-18 19:08:06 -0300

    fixed packer auto-install

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,7 +2,7 @@
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.api.nvim_command("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+	vim.api.nvim_command("!git clone --depth 1 https://github.com/wbthomason/packer.nvim " .. install_path)
 	vim.api.nvim_command "packadd packer.nvim"
 end
 
@@ -47,7 +47,4 @@ return require('packer').startup(function (use)
 
 	-- Terminal
 	use 'akinsho/toggleterm.nvim'
-
-	-- CoPilot
-	use 'github/copilot.vim'
 end)
